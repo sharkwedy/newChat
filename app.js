708d652186d94f4a985dda35799f2e6335a3c99c
¨Thales¨ <talls.urin@hotmail.com>
2018-10-14 17:53:35 -0300

    ¨a¨

--- app.js
+++ app.js
@@ -26,8 +26,49 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', loginRouter);
 app.use('/newUser', newUserRouter);
 
-app.listen(app.get('port'), () => {
+var server = app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* criar a conexão por websocket */
+io.on('connection', function(socket){
+	console.log('Usuário conectou');
+
+	socket.on('disconnect', function(){
+		console.log('Usuário desconectou');
+	});
+
+	socket.on('msgParaServidor', function(data){
+
+		/* dialogo */
+		socket.emit(
+			'msgParaCliente', 
+			{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+		socket.broadcast.emit(
+			'msgParaCliente', 
+			{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+		/* participantes */
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit(
+				'participantesParaCliente', 
+				{apelido: data.apelido}
+			);
+
+			socket.broadcast.emit(
+				'participantesParaCliente', 
+				{apelido: data.apelido}
+			);
+		}
+	});
+
 });
 
 module.exports = app;
